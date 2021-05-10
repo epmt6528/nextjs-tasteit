9db14d77c21b945858c02e8a21adc5b76d60ff4f
Taichi Murai <epmt6528@gmail.com>
2021-05-10 10:33:19 -0700

    Added meta info

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import { AppProps } from 'next/app'
+import Link from 'next/link'
+import '../styles/sanitize.css'
 import '../styles/globals.css'
 
+import Amplify from 'aws-amplify'
+import config from '../aws-exports'
+Amplify.configure({
+  ...config,
+  ssr: true,
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/profile">Profile</Link>
+        <Link href="/protected">Protected</Link>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
